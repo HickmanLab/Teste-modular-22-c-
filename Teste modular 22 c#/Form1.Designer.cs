b950d9afaee36ec040685889c5ef7a23a016bc3d
LAB_PC_09 <LAB_PC_09@LAB_PC_09>
2024-05-16 21:41:41 -0300

    Adicionar arquivos de projeto.

---
+++ Teste modular 22 c#/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Teste_modular_22_c_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            comboBox1 = new ComboBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Times New Roman", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(217, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(364, 32);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "Olá mundo! Eu sou o Teste Modula 22";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Font = new Font("Times New Roman", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(289, 133);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(148, 23);
+            comboBox1.TabIndex = 1;
+            comboBox1.Text = "Escolha uma opção";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(800, 450);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private ComboBox comboBox1;
+    }
+}
